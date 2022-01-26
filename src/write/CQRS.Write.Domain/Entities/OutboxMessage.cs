7e1be2896999734b6a5024b075f7d79c9cd55607
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-26 23:24:40 +0300

    - outbox message schema changed.

--- src/write/CQRS.Write.Domain/Entities/OutboxMessage.cs
+++ src/write/CQRS.Write.Domain/Entities/OutboxMessage.cs
@@ -6,5 +6,7 @@
         public DateTime OccurredOn { get; set; }
         public string EventType { get; set; }
         public string Payload { get; set; }
+        public bool IsPublished { get; set; }
+        public DateTime? PublishedDate { get; set; }
     }
 }
