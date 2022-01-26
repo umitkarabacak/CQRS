8af2d5eb53c8112b5a8f6fa159112589de52ce34
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-26 13:15:33 +0300

    - cqrs write basic category management completed.

---
+++ src/write/CQRS.Write.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using CQRS.Write.Application.Categories.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS.Write.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ILogger<CategoriesController> _logger;
+        private readonly IMediator _mediator;
+
+        public CategoriesController(ILogger<CategoriesController> logger
+            , IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CategoryCreateRequest categoryCreateRequest)
+        {
+            var response = await _mediator.Send(categoryCreateRequest);
+
+            return Ok(response);
+        }
+    }
+}
