c97b501435e994a037564a23e8e408beae5b0600
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-26 11:35:39 +0300

    - cqrs write infrastructure layer created.

---
+++ src/write/CQRS.Write.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿global using CQRS.Write.Domain.Entities;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.Extensions.Configuration;
+
+namespace CQRS.Write.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ProjectContext>(opt =>
+                opt.UseSqlServer(configuration.GetConnectionString("Default"))
+            );
+
+            return services;
+        }
+    }
+}
---
+++ src/write/CQRS.Write.Infrastructure/ProjectContext.cs
@@ -0,0 +1,15 @@
+﻿namespace CQRS.Write.Infrastructure
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OutboxMessage> OutboxMessages { get; set; }
+    }
+}
