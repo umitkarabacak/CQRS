bfda0e3b368975b5f9297bb466c0b6da3008f95f
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-28 01:09:41 +0300

    change created event models

--- src/consumers/CQRS.CategoryConsumer.App/Events/CategoryCreatedEvent.cs
+++ src/consumers/CQRS.CategoryConsumer.App/Events/CategoryCreatedEvent.cs
@@ -2,7 +2,7 @@
 {
     public class CategoryCreatedEvent
     {
-        public Guid Id => Guid.NewGuid();
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
