0e266512d69341c28e9ec5d0a7c6881e1f0d6183
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-27 22:50:45 +0300

    - cqrs read category default project created.

---
+++ src/read/CQRS.ReadCategory.Api/Program.cs
@@ -0,0 +1,39 @@
+using Nest;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region Elastic Search
+
+var settings = new ConnectionSettings();
+    settings.DefaultIndex("categories");
+    //settings.BasicAuthentication("username","pwd");
+
+var elasticClient = new ElasticClient(settings);
+
+builder.Services.AddSingleton(elasticClient);
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
