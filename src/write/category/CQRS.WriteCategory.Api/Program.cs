859ccd599f5975b7238d94344ba6fce3bfa0f3a0
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-28 00:49:47 +0300

    cqrs write category api layer created.

---
+++ src/write/category/CQRS.WriteCategory.Api/Program.cs
@@ -0,0 +1,35 @@
+using CQRS.WriteCategory.Application;
+using CQRS.WriteCategory.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<ProjectContext>();
+    var response = await context.Database.EnsureCreatedAsync();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
