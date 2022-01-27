4e29967465665a0ba6c9dcc06fb0eb5c2d69de4f
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-28 00:46:46 +0300

    cqrs write category infrastructure layer created.

---
+++ src/write/category/CQRS.WriteCategory.Infrastructure/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿namespace CQRS.WriteCategory.Infrastructure
+{
+    public class ProjectContext : DbContext, IProjectContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<OutboxMessage> OutboxMessages { get; set; }
+    }
+}
