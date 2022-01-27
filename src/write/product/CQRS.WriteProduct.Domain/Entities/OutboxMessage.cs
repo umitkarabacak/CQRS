aa02e804107ea8d95676d2a034eeb6007f3ae661
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-28 00:53:07 +0300

    cqrs write product domain layer created.

---
+++ src/write/product/CQRS.WriteProduct.Domain/Entities/OutboxMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace CQRS.WriteProduct.Domain.Entities
+{
+    public class OutboxMessage
+    {
+        public Guid Id { get; set; }
+        public DateTime OccurredOn { get; set; }
+        public string EventType { get; set; }
+        public string Payload { get; set; }
+        public bool IsPublished { get; set; }
+        public DateTime? PublishedDate { get; set; }
+    }
+}
