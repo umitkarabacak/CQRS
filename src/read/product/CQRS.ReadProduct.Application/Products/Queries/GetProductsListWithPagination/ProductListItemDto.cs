d643b4ec0305b5779729f19fcf64c2c15d2f0fb7
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-28 01:56:42 +0300

    cqrs read product tested

--- src/read/product/CQRS.ReadProduct.Application/Products/Queries/GetProductsListWithPagination/ProductListItemDto.cs
+++ src/read/product/CQRS.ReadProduct.Application/Products/Queries/GetProductsListWithPagination/ProductListItemDto.cs
@@ -2,7 +2,7 @@
 {
     public class ProductListItemDto
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public double? UnitPrice { get; set; }
     }
